cbcd5333e6e5c38d0ce512060d999e0a8c72b61d
Ahsan <shakilahsan45@gmail.com>
2021-08-02 19:50:34 +0600

    version_2

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,14 @@
 const project__card = document.querySelectorAll(".subhead");
 const project__div = document.querySelectorAll(".project__div");
+const all__projects = document.querySelectorAll(".all__projects");
 
 project__card.forEach((item) => {
     item.addEventListener("click", function () {
         for (let i = 0; i < project__div.length; i++) {
             project__div[i].classList.remove("block");
+            all__projects[i].classList.remove("allow");
         }
         item.nextElementSibling.classList.add("block");
+        item.parentElement.classList.add("allow");
     });
 });
